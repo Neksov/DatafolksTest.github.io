0c860d9e8cf109b30b2745579855912c3957cbbb
Neksov <n2v@list.ru>
2020-09-11 10:45:12 +0300

    add mobile

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
-})
+$("#myModal").on("shown.bs.modal", function () {
+  $("#myInput").trigger("focus");
+});
+//анимация
+new WOW().init();
