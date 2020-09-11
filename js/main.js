356c9943ded8dbb40cf51a5492ff2b0bd6446ae1
Neksov <n2v@list.ru>
2020-09-11 13:24:03 +0300

    add form

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $("#myModal").on("shown.bs.modal", function () {
   $("#myInput").trigger("focus");
 });
+
+//маска для номера телефона
+$("[type=tel]").mask("+7(000)000-00-00", {
+  placeholder: "Ваш номер телефона:",
+});
